c072fc5e74742c5df0921fd6fbb8500d896d7eee
joonghyun <joongh@ewhain.net>
2023-09-16 12:53:38 +0900

    #6 feat: 질문 등록 API

--- src/main/java/com/ewhatever/qna/answer/controller/AnswerController.java
+++ src/main/java/com/ewhatever/qna/answer/controller/AnswerController.java
@@ -1,9 +1,31 @@
 package com.ewhatever.qna.answer.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ewhatever.qna.answer.dto.PostAnswerReq;
+import com.ewhatever.qna.answer.repository.AnswerRepository;
+import com.ewhatever.qna.answer.service.AnswerService;
+import com.ewhatever.qna.common.Base.BaseException;
+import com.ewhatever.qna.common.Base.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import static com.ewhatever.qna.common.Base.BaseResponseStatus.SUCCESS;
 
 @RestController
-@RequestMapping
+@RequestMapping("/answers")
+@RequiredArgsConstructor
 public class AnswerController {
+    AnswerService answerService;
+    /**
+     * 답변 등록
+     */
+    @ResponseBody
+    @PostMapping("")
+    public BaseResponse<String> addAnswer(@RequestBody PostAnswerReq postAnswerReq, Long userIdx) {
+        try {
+            answerService.addAnswer(userIdx, postAnswerReq); //TODO: authService.getUserIdx()로 수정
+            return new BaseResponse<>(SUCCESS);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 }
--- src/main/java/com/ewhatever/qna/answer/service/AnswerService.java
+++ src/main/java/com/ewhatever/qna/answer/service/AnswerService.java
@@ -1,7 +1,42 @@
 package com.ewhatever.qna.answer.service;
 
+import com.ewhatever.qna.answer.dto.PostAnswerReq;
+import com.ewhatever.qna.answer.entity.Answer;
+import com.ewhatever.qna.answer.repository.AnswerRepository;
+import com.ewhatever.qna.common.Base.BaseException;
+import com.ewhatever.qna.post.entity.Post;
+import com.ewhatever.qna.post.repository.PostRepository;
+import com.ewhatever.qna.user.entity.User;
+import com.ewhatever.qna.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import static com.ewhatever.qna.common.Base.BaseResponseStatus.*;
+import static com.ewhatever.qna.common.Constant.ACTIVE;
 
 @Service
 public class AnswerService {
+    UserRepository userRepository;
+    PostRepository postRepository;
+    AnswerRepository answerRepository;
+    /**
+     * 답변 등록
+     */
+    public void addAnswer(Long userIdx, PostAnswerReq postAnswerReq) throws BaseException {
+        try {
+            User user = userRepository.findByUserIdxAndStatusEquals(userIdx, ACTIVE);
+            Post post = postRepository.findById(postAnswerReq.getPostIdx()).orElseThrow(() -> new BaseException(INVALID_POST_IDX));
+            if (user.getRole().equals("Senior")) {//TODO: enum으로 변경
+                Answer answer = Answer.builder()
+                        .content(postAnswerReq.getAnswer())
+                        .answerer(user)
+                        .post(post)
+                        .build();
+                answerRepository.save(answer);
+            } else throw new BaseException(NO_SENIOR_ROLE);
+        } catch (BaseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
 }
