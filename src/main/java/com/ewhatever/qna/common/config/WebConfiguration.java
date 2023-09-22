e99f06dd63d9a42019b32eec6bac60adee519f2d
Haeun-Y <hahaha329@ewhain.net>
2023-09-22 20:30:18 +0900

    #28 fix: CORS 설정-프론트 배포 url 허용

--- src/main/java/com/ewhatever/qna/common/config/WebConfiguration.java
+++ src/main/java/com/ewhatever/qna/common/config/WebConfiguration.java
@@ -10,7 +10,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") //TODO : 프론트 베포 URL 추가
+                .allowedOrigins("http://localhost:3000", "https://ewhatever-front.vercel.app")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE");
     }
 }
