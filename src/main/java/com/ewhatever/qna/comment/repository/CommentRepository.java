5ef8891ec2977aee91145d782e0520ed0a30b671
joonghyun <joongh@ewhain.net>
2023-09-25 09:21:45 +0900

    #3 fix: 댓글 정렬 기준 수정

--- src/main/java/com/ewhatever/qna/comment/repository/CommentRepository.java
+++ src/main/java/com/ewhatever/qna/comment/repository/CommentRepository.java
@@ -11,5 +11,5 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Long countByWriter_UserIdxAndStatusEquals(Long userIdx, String status);
     Page<Comment> findByWriter_UserIdxAndStatusEquals(Long userIdx, String status, Pageable pageable);
-    List<Comment> findAllByPostAndStatusEqualsOrderByCreatedDateDesc(Post post, String status);
+    List<Comment> findAllByPostAndStatusEqualsOrderByCreatedDate(Post post, String status);
 }
